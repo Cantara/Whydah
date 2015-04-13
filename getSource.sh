5dfbcc06156422a74735a567eff07bda94777c62
Thor Henning Hetland <totto@totto.org>
2015-04-13 14:07:17 +0200

    added getSource to simplify downloading all key Whydah modules

---
+++ getSource.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd ..
+git clone git@github.com:Cantara/Whydah-UserIdentityBackend.git
+git clone git@github.com:Cantara/Whydah-SecurityTokenService.git
+git clone git@github.com:Cantara/Whydah-SSOLoginWebApp.git
+git clone git@github.com:Cantara/Whydah-UserAdminService.git
+git clone git@github.com:Cantara/Whydah-UserAdminWebApp.git
+
