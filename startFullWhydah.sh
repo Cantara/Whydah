3fb404cf093c5ae4e76d9ae2cb86bb15a8a47459
Thor Henning Hetland <totto@totto.org>
2015-04-14 18:38:57 +0200

    caps Version

--- startFullWhydah.sh
+++ startFullWhydah.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ../Whydah-SecurityTokenService
-VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
+Version=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
 cd ../Whydah-SSOLoginWebApp
-VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
+Version=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
 cd ../Whydah-UserAdminService
-VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
+Version=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
 cd ../Whydah-UserAdminWebApp
-VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
+Version=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
 cd ../Whydah-UserIdentityBackend
-VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
+Version=FROM_SOURCE IAM_MODE=TEST_LOCALHOST ./start-service.sh 
 
