1db82c2bafc65953c5fcfed3f4a0bea19965b058
Thor Henning Hetland <totto@totto.org>
2015-04-14 10:15:13 +0200

    Using parameter=value to simplify parsing

--- startFullWhydah.sh
+++ startFullWhydah.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 cd ../Whydah-SecurityTokenService
-./start-service.sh FROM_SOURCE TEST_LOCALHOST
+./start-service.sh VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST
 cd ../Whydah-SSOLoginWebApp
-./start-service.sh FROM_SOURCE TEST_LOCALHOST
+./start-service.sh VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST
 cd ../Whydah-UserAdminService
-./start-service.sh FROM_SOURCE TEST_LOCALHOST
+./start-service.sh VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST
 cd ../Whydah-UserAdminWebApp
-./start-service.sh FROM_SOURCE TEST_LOCALHOST
+./start-service.sh VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST
 cd ../Whydah-UserIdentityBackend
-./start-service.sh FROM_SOURCE TEST_LOCALHOST
+./start-service.sh VERSION=FROM_SOURCE IAM_MODE=TEST_LOCALHOST
 
