b81b1dee5f847167231101bb54fc0ca06acb17ba
Thor Henning Hetland <totto@totto.org>
2015-04-23 11:51:15 +0200

    added some background info in script

--- config/Docker/startWhyhadinstancesOnPrimaryDockerHost.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Lets start three STS instances in the sts-mesh
-sudo socketplane run -n sts-mesh -it cantara/whydah-sts
-sudo socketplane run -n sts-mesh -it cantara/whydah-sts
-sudo socketplane run -n sts-mesh -it cantara/whydah-sts
-
