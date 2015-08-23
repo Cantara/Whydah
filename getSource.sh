bca62f9af0f154a265f6987eadae406de976cbb4
Sherriff <erik-dev@fjas.no>
2015-08-23 13:09:32 +0200

    remove the old scripts, moved to folder

--- getSource.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd ..
-git clone https://github.com/Cantara/Whydah-UserIdentityBackend.git
-git clone https://github.com/Cantara/Whydah-SecurityTokenService.git
-git clone https://github.com/Cantara/Whydah-SSOLoginWebApp.git
-git clone https://github.com/Cantara/Whydah-UserAdminService.git
-git clone https://github.com/Cantara/Whydah-UserAdminWebApp.git
-
