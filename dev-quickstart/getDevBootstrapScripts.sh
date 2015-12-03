09ccb48312066d966f0c34265a0fd55a2264ef43
Thor Henning Hetland <totto@totto.org>
2015-12-03 21:46:36 +0100

    Create getDevBootstrapScripts.sh

---
+++ dev-quickstart/getDevBootstrapScripts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/getSource.sh
+wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/buildFullWhydah.sh
+wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/startFullWhydah.sh
