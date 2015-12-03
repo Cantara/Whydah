2692df5cd8d5c1a57dde1f3c46b1b3c6a70d8289
Thor Henning Hetland <totto@totto.org>
2015-12-03 22:20:42 +0100

    Update getDevBootstrapScripts.sh

--- dev-quickstart/getDevBootstrapScripts.sh
+++ dev-quickstart/getDevBootstrapScripts.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+echo "Tis script will download scripts, whydah source, build from source and start Whydah"
+
 wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/getSource.sh
 wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/buildFullWhydah.sh
 wget https://raw.githubusercontent.com/Cantara/Whydah/master/dev-quickstart/startFullWhydah.sh
 chmod 755 *.sh
+getSource.sh
+buildFullWhydah.sh
+startFullWhydah.sh
