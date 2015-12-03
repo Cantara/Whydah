8371c1a4b6d37f906e64511e8635cddbc2b8c2f0
Thor Henning Hetland <totto@totto.org>
2015-12-03 21:32:34 +0100

    Update getSource.sh

--- dev-quickstart/getSource.sh
+++ dev-quickstart/getSource.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 #cd ..
 git clone https://github.com/Cantara/Whydah-UserIdentityBackend.git uib
+cd uib
+ln -s src/main/resources/prodInitData/ prodInitData
+cd ..
 git clone https://github.com/Cantara/Whydah-SecurityTokenService.git sts
-git clone https://github.com/Cantara/Whydah-SSOLoginWebApp.git ssologin
+git clone https://github.com/Cantara/Whydah-SSOLoginWebApp.git ssolwa
 git clone https://github.com/Cantara/Whydah-UserAdminService.git uas
 git clone https://github.com/Cantara/Whydah-UserAdminWebApp.git uawa
 
