1923c30bacb4a582cda212be67b89f93e0e5e177
Thor Henning Hetland <totto@totto.org>
2015-12-03 22:22:58 +0100

    Update startFullWhydah.sh

--- dev-quickstart/startFullWhydah.sh
+++ dev-quickstart/startFullWhydah.sh
@@ -20,6 +20,6 @@ Version=FROM_SOURCE env_vars='-DCONSTRETTO_TAGS=DEV' ./start-service.sh
 sleep 15
 echo ""
 echo ""
-echo "To test point your browser to:  http://localhost:9997/sso/welcome   u:admin pw:admin"
+echo "To test point your browser to:  http://localhost:9997/sso/welcome   u:useradmin pw:useradmin"
 
 
