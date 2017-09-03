a27d090b21aaf4f5ed7629fafc1fa6639f460cf9
totto <totto@totto.org>
2017-09-03 12:17:29 +0200

    minor update

--- scripts/server-automation-example (ec2-aws linux)/upgradeAllToLatest.sh
+++ scripts/server-automation-example (ec2-aws linux)/upgradeAllToLatest.sh
@@ -15,13 +15,19 @@
 sudo /etc/init.d/postgresql restart
 
 
-# and get the others services back on-line
-/home/ec2-user/restart-ConfigService.sh
-/home/ec2-user/restart-CS-Dashboard.sh
-/home/ec2-user/restart-JAU2.sh
-/home/ec2-user/restart-JAU.sh
-
 
 # And check that things are starting
+echo "TokenService"
 wget -qO- http://localhost:9998/tokenservice/health
+echo "SSOLWA"
 wget -qO- http://localhost:9997/sso/health
+echo "UAS"
+wget -qO- http://localhost:9992/useradminservice/health
+echo "UAWA"
+wget -qO- http://localhost:9996/useradmin/health
+echo "StatisticsService"
+wget -qO- http://localhost:4901/reporter/health
+echo "CRMService"
+wget -qO- http://localhost:12121/crmservice/health
+echo "OAuth2"
+wget -qO- http://localhost:9898/oauth2/health
