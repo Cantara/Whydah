df4fc34e0cb7d161b9bdcdb45e9772630fe8293b
Thor Henning Hetland <totto@totto.org>
2018-04-04 13:11:48 +0200

    added missing modules and fixed typos

--- dev-quickstart/getSource.sh
+++ dev-quickstart/getSource.sh
@@ -7,11 +7,12 @@ ln -s src/test/resources/testdata/ testdata
 cd ..
 git clone https://github.com/Cantara/Whydah-TypeLib.git 
 git clone https://github.com/Cantara/Whydah-Java-SDK.git 
+git clone https://github.com/Cantara/Whydah-Admin-SDK.git
 git clone https://github.com/Cantara/Whydah-SecurityTokenService.git 
 git clone https://github.com/Cantara/Whydah-SSOLoginWebApp.git 
 git clone https://github.com/Cantara/Whydah-UserAdminService.git 
 git clone https://github.com/Cantara/Whydah-UserAdminWebApp.git 
 git clone https://github.com/Cantara/Whydah-CRMService.git 
 git clone https://github.com/Cantara/Whydah-StatisticsService.git 
-git clone https://github.com/Cantara/Whydah-SPAProxyService
-git clone https://github.com/Cantara/Whydah-OAuth2Service
+git clone https://github.com/Cantara/Whydah-SPAProxyService.git
+git clone https://github.com/Cantara/Whydah-OAuth2Service.git
