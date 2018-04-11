b050e4b32f36c85c09747ac02a741376e65d541a
Thor Henning Hetland <totto@totto.org>
2018-04-11 10:26:38 +0200

    Create getSourceHttp.sh

---
+++ dev-quickstart/getSourceHttp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#cd ..
+git clone https://github.com/Cantara/Whydah-UserIdentityBackend.git 
+cd Whydah-UserIdentityBackend
+ln -s src/main/resources/prodInitData/ prodInitData
+ln -s src/test/resources/testdata/ testdata
+cd ..
+git clone https://github.com/Cantara/Whydah-TypeLib.git 
+git clone https://github.com/Cantara/Whydah-Java-SDK.git 
+git clone https://github.com/Cantara/Whydah-Admin-SDK.git
+git clone https://github.com/Cantara/Whydah-SecurityTokenService.git 
+git clone https://github.com/Cantara/Whydah-SSOLoginWebApp.git 
+git clone https://github.com/Cantara/Whydah-UserAdminService.git 
+git clone https://github.com/Cantara/Whydah-UserAdminWebApp.git 
+git clone https://github.com/Cantara/Whydah-CRMService.git 
+git clone https://github.com/Cantara/Whydah-StatisticsService.git 
+git clone https://github.com/Cantara/Whydah-SPAProxyService.git
+git clone https://github.com/Cantara/Whydah-OAuth2Service.git
